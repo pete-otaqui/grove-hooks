0d4504cac25496e992320bb1c727031a5125ed0d
Pete Otaqui <pete@otaqui.com>
2019-03-13 15:04:00 +0000

    Tidied up indentation

--- src/github-components/github-search/github-search.js
+++ src/github-components/github-search/github-search.js
@@ -1,8 +1,8 @@
-/* 
+/*
  * GitHub Search Component
- * 
+ *
  * This component uses react's `useState` hook, and the `fetch` api.
- * 
+ *
  * We expect a function to be given as the child of this component, in the
  * "render props" pattern.  This function will be passed the following props:
  *
@@ -35,7 +35,7 @@
  * }
  *
  * ```
- * 
+ *
  * Note that it contains no UI or even JSX, so we don't actually have to import
  * React as normal - only the hook.
  */
@@ -46,42 +46,40 @@ import PropTypes from "prop-types";
 const defaultResult = { items: [], total_count: 0 };
 
 export default function GithubSearch({ children }) {
+  const [result, setResult] = useState(defaultResult);
+  const [isSearching, setIsSearching] = useState(false);
 
-  const [ result, setResult ] = useState(defaultResult);
-  const [ isSearching, setIsSearching ] = useState(false);
-	
-	// create a function to pass through as render prop,
-	// which will trigger a search with params
-	const search = async (params) => {
-		// set state that searching is active
+  // create a function to pass through as render prop,
+  // which will trigger a search with params
+  const search = async params => {
+    // set state that searching is active
     setIsSearching(true);
     try {
-			// should use a library for constructing a query
+      // should use a library for constructing a query
       const query = Object.entries(params)
-				.map(([k,v]) => `${k}=${v}`) 
-				.join("=");
+        .map(([k, v]) => `${k}=${v}`)
+        .join("=");
 
-			// use the `fetch` api to get the data, convert to json
+      // use the `fetch` api to get the data, convert to json
       const url = `https://api.github.com/search/repositories?${query}`;
       const response = await fetch(url);
       const json = await response.json();
 
-			// set the results
+      // set the results
       setResult(json);
-		} catch(e) {
-			// if we had an error, reset the result to default
-			setResult(defaultResult);
+    } catch (e) {
+      // if we had an error, reset the result to default
+      setResult(defaultResult);
     } finally {
-			// whether we errored or not, we aren't searching any more
+      // whether we errored or not, we aren't searching any more
       setIsSearching(false);
     }
-  }
-	// cool - let's run the render child fn with props
+  };
+  // cool - let's run the render child fn with props
   return children({ result, search, isSearching });
 }
 
 GithubSearch.propTypes = {
-  children: PropTypes.func.isRequired,
+  children: PropTypes.func.isRequired
 };
 GithubSearch.defaultProps = {};
-
